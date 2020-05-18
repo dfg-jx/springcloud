a34a2fa97f5aa72cac04ee9d1c870a1ba32d1254
dengfugui <deng_fg@uxsino.com>
2020-05-18 17:26:52 +0800

    dfg: 使用网络配置

--- compute-service/src/main/java/com/dfg/computeservice/controller/TestController.java
+++ compute-service/src/main/java/com/dfg/computeservice/controller/TestController.java
@@ -1,13 +1,13 @@
 package com.dfg.computeservice.controller;
 
+import com.dfg.computeservice.entity.GitAutoRefreshConfig;
+import com.dfg.computeservice.entity.GitConfig;
 import com.sun.istack.logging.Logger;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
@@ -16,6 +16,12 @@ public class TestController {
     @Autowired
     private DiscoveryClient client;
 
+    @Autowired
+    private GitConfig gitConfig;
+
+    @Autowired
+    private GitAutoRefreshConfig gitAutoRefreshConfig;
+
     @RequestMapping(value = "/add" ,method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
         ServiceInstance instance = client.getInstances(client.getServices().get(0)).get(0);
@@ -23,4 +29,22 @@ public class TestController {
         logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
         return r;
     }
+
+
+    @GetMapping(value = "/show")
+    public Object show(){
+        String env = gitConfig.getEnv();
+        String password = gitConfig.getPassword();
+        String username = gitConfig.getUsername();
+        return gitConfig;
+    }
+
+    @GetMapping(value = "/autoShow")
+    public Object autoShow(){
+        String env = gitAutoRefreshConfig.getEnv();
+        String password = gitAutoRefreshConfig.getUser().getUsername();
+        String username = gitAutoRefreshConfig.getUser().getUsername();
+        return gitAutoRefreshConfig;
+    }
+
 }
